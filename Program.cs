711fd73e7ea9c336724778ddf499ac5e6da09049
Anh Nguyen <tuananhnguyen121020@gmail.com>
2022-11-14 15:23:06 +0700

    Sub Branch by Tuan Anh

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
 ﻿using System;
-
+using testLib;
 namespace std
 {
     class Program
     {
         public static void Main(string[] args){
-            Console.WriteLine("Co cai con cak");
+            int a = 10;
+            int b = 11;
+            
+
+            Console.Write(Sum.Calculate(12,12));
+
         }
+
+        
+        // public static int Sum(int a, int b){
+        //     return a + b;
+        // }
     }
 }
---
+++ testLib.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace testLib{
+    public class Sum{
+        public static int a = 10;
+        public static int Calculate (int a, int b){
+            return a + b;
+        }
+    }
+    
+}
+
+
+
+
+
