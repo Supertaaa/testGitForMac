3876d74ca6fac8cf8c6490cf6bd32f932deb8733
Anh Nguyen <tuananhnguyen121020@gmail.com>
2022-11-15 11:04:02 +0700

    Thu muc moi ne

---
+++ testCSharp/testLib.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace testLib{
+    class Sum{
+        public static int a = 10;
+        public static int AddOperator (int a, int b){
+            return a + b;
+        }
+        public static int MinusOperator(int a, int b){
+            return a - b;
+        }
+
+        public static int TimeOperator(int a, int b){
+            return a * b;
+        }
+        public static double DivOperator(int a, int b){
+            return a / b;
+        }
+
+
+    }
+    
+}
+
+
+
+
+
